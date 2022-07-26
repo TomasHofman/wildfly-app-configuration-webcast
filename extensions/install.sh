2e80ca43a3363cdc7c9779af9d6327fc6e143145
Tomas Hofman <thofman@redhat.com>
2022-07-26 08:16:33 +0200

    Remove the standalone_xml_history/current dir, prevent Wildfly error at runtime

--- extensions/install.sh
+++ extensions/install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
-source /usr/local/s2i/install-common.sh
+
 injected_dir=$1
 
 echo "Applying ${injected_dir}/wildfly-buildtime-config.cli"
 $JBOSS_HOME/bin/jboss-cli.sh --file="${injected_dir}/wildfly-buildtime-config.cli"
+
+# Delete configuration history, because in container environment Wildfly would not be able to move this directory anymore at run-time.
+rm -fr $JBOSS_HOME/standalone/configuration/standalone_xml_history/current
